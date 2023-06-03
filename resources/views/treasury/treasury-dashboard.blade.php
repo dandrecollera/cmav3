3db93a2853876c32e01cf91c1345614e2ab83801
zeesaa <lozadazeesa@gmail.com>
2023-06-03 10:07:12 +0800

    dashboard at reports

--- resources/views/treasury/treasury-dashboard.blade.php
+++ resources/views/treasury/treasury-dashboard.blade.php
@@ -61,15 +61,15 @@
                                 </div>
                             </div>
 
-                            <div class="col-xl-3 col-md-6">
+                            {{--<div class="col-xl-3 col-md-6">
                                 <div class="card bg-dark text-white mb-4">
                                     <div class="card-body">Weekly Sales</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         {{--<a class="small text-white stretched-link" href="#">View Details</a>--}}
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i> ₱ {{--  number_format((float)$dashboardInfo['weeklySales'], 2, '.', '')--}}</div>
+                                        {{--<div class="small text-white"><i class="fas fa-angle-right"></i> ₱ {{  number_format((float)$dashboardInfo['weeklySales'], 2, '.', '')}}</div>
                                     </div>
                                 </div>
-                            </div>
+                            </div>--}}
 
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-dark text-white mb-4">
