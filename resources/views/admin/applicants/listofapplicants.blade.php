3a002b2a328165bcfb455b55129080be23ef3bbd
zeesaa <lozadazeesa@gmail.com>
2023-06-08 14:06:18 +0800

    applicant

--- resources/views/admin/applicants/listofapplicants.blade.php
+++ resources/views/admin/applicants/listofapplicants.blade.php
@@ -95,7 +95,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Applicant's Info</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Applicant</h5>
                 <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
