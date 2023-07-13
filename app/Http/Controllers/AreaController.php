611ad6824fdaf276905b64e554e70fc722ffe791
zeesaa <lozadazeesa@gmail.com>
2023-07-13 20:29:58 +0800

    push ulit

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -12,7 +12,7 @@ class AreaController extends Controller
 {
     public function dropdown(Request $request)
     {
-        
+        //floor and section table
         $area = DB::table('floors')
         ->leftjoin('sections', 'floors.floornum', '=', 'sections.floornum')
         ->select('sections.section', 'sections.floornum', 'floors.id')
